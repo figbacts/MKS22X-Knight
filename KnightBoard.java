e3db50352920b4a6e40bb864a2c68ba83080f0f0
Jack Lam <jlam00@stuy.edu>
2019-02-14 11:30:20 -0500

    add and remove work

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,16 +1,16 @@
 public class KnightBoard{
   private int board[][];
-  private int move[][];
+  //private int move[][];
   public KnightBoard(int startingRows, int startingCols){
     //if (startingCols <= 0 || startingRows <= 0){
     //  throw new IllegalArgumentException e;
     //}
   board = new int[startingRows][startingCols];
-  move = new int[2][2];
-  move[0][0] = 1;
-  move[0][1] = -1;
-  move[1][0] = 2;
-  move[1][1] = -2;
+  //move = new int[2][2];
+  //move[0][0] = 1;
+  //move[0][1] = -1;
+  //move[1][0] = 2;
+  //move[1][1] = -2;
 }
   public String toString(){
     String ans = "";
@@ -28,23 +28,36 @@ public class KnightBoard{
       }
     ans += "\n";
     }
-    ans += move[0][0] + move[0][1] + move[1][0] + move[1][1];
+    //ans += move[0][0] + move[0][1] + move[1][0] + move[1][1];
     return ans;
   }
 
   public boolean solve(int startingRows, int startingCols){
     board[startingRows][startingCols] = 1;
     return helper(startingRows, startingCols, 2);
-
   }
   public boolean helper(int row, int col, int index){
-    if (index == row * col){
-      return true;
+    return true;
+  }
+  private boolean add(int row, int col, int index){
+    if (row < 0 || col < 0 || row >= board.length || col >= board[0].length){
+      return false;
     }
-    return false;
+    board[row][col] = index;
+    return true;
+  }
+  private boolean remove(int row, int col){
+    if (row < 0 || col < 0 || row >= board.length || col >= board[0].length){
+      return false;
+    }
+    board[row][col] = 0;
+    return true;
   }
   public static void main(String[] args) {
     KnightBoard board = new KnightBoard(5,5);
+    board.add(0,0,1);
+    board.add(0,1,21);
+    //board.remove(0,1);
     System.out.println(board);
   }
 }
